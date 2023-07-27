14fce040b3a06a8ecff608f1fa73c709922ba0e5
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-27 15:55:20 -0400

    added css and functions to store new tasks on the browser in locale storage

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //Todo List
+const task = document.getElementById('taskBox');
+const newLIst = document.getElementById('taskList');
+let myArray = [];
+
+function addTask(){
+	let li = document.createElement('li');
+	li.innerHTML = task.value;
+	newLIst.appendChild(li);
+	let errand = task.value
+
+	task.value = '';
+
+	let myObject = {
+		item: errand,
+		id: Math.floor(Math.random() * 100)
+	};
+	myArray.push(myObject)
+
+	addItem();
+}
+
+function addItem(){
+	let objString = JSON.stringify(myArray);
+	localStorage.setItem("todo", newLIst.innerHTML);
+}
+
+function showItem(){
+	newLIst.innerHTML = localStorage.getItem("todo");
+}
+showItem();
